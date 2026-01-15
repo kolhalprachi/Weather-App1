f7d72e6cfe0d689de6188f489bf484009a933f31
kolhalprachi <166357057+kolhalprachi@users.noreply.github.com>
2026-01-15 15:33:29 +0530

    Delete WheatherApi directory

--- WheatherApi/script.js
+++
@@ -1,8 +0,0 @@
-let cityName=prompt("Enter the city name");
-let fetchApi=async ()=>{
-    let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=pune&appid=06a34d287c1333589089519c4ff35e3e`);
-    data=await data.json();
-    console.log(data);
-
-}
-fetchApi();
